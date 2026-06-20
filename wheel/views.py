4034dbf21bd1a91aeec192d56d5f65fe3adb5c1b
Henok <henokabesha57@gmail.com>
2026-06-20 20:55:02 +0300

    Update views.py

--- wheel/views.py
+++ wheel/views.py
@@ -1,37 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_http_methods, require_GET, require_POST
-from django.contrib.auth.decorators import login_required
 import json
-from .models import WheelEntry, WheelSession
 
 def index(request):
-    session, _ = WheelSession.objects.get_or_create(id=1)
-    entries = session.entries.all()
-    
-    # Get the last winner from the session
-    last_winner = session.current_winner.name if session.current_winner else None
-    
-    context = {
-        'entries': entries,
-        'entries_json': json.dumps([{
-            'id': e.id,
-            'name': e.name,
-            'color': e.color,
-            'weight': e.weight
-        } for e in entries]),
-        'total_spins': session.spin_count,
-        'last_winner': last_winner
-    }
-    return render(request, 'index.html', context)
 
-@login_required
+    return render(request, 'index.html')
+
 def admin_panel(request):
-    session, _ = WheelSession.objects.get_or_create(id=1)
-    entries = session.entries.all()
-    
-    context = {
-        'session': session,
-        'entries': entries
-    }
-    return render(request, 'admin_panel.html', context)
+    return render(request, 'admin_panel.html')
